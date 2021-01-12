6d23d3e73e3bc0cd5b52b2debbdff227f26e50cb
MikeMcflako <mike.mcflako@gmail.com>
2021-01-12 01:08:26 -0600

    Se crea el archivo de la página de información del usuario

---
+++ src/views/user.js
@@ -0,0 +1,12 @@
+import { Page } from "../components"
+
+const User = () => {
+    return (
+        <Page 
+            id="user-info"
+            title="User info"
+        ></Page>
+    )
+}
+
+export default User;
