7abbb8d6a11e117a77b7cc7f5ab22a2784de383d
MikeMcflako <mike.mcflako@gmail.com>
2021-01-11 23:38:43 -0600

    Se añade caso por defecto

--- src/utils/get-request-error-message.js
+++ src/utils/get-request-error-message.js
@@ -2,6 +2,8 @@ const getRequestErrorMessage = (code, type) => {
     switch(code){
         case 404:
             return `The ${type} you are looking for could not be found`
+        default:
+            return 'An unexpected error has occurred'
     }
 }
 
